77418cc5e3b7c5aca88653e23bd9a0d8bfee8ecb
JP Tomberlin <jp.tomberlin@gmail.com>
2016-02-17 19:51:02 -0700

    Initial Commit

---
+++ Tap Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tap Me
+//
+//  Created by JP TOMBERLIN on 2/17/16.
+//  Copyright © 2016 JP TOMBERLIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
